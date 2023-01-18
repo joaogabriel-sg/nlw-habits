9287688aeed4463af436301d7a730ce88f02350b
João Gabriel <main.joaogabrielsg@gmail.com>
2023-01-17 21:07:51 -0300

    feat(web): setup

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import './styles/global.css'
+
+import { Habit } from "./components/Habit"
+
+function App() {
+  return (
+    <div>
+      <Habit completed={3} />
+      <Habit completed={10} />
+      <Habit completed={31} />
+    </div>
+  )
+}
+
+export default App
