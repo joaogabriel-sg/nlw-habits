c3609c14f695b8796366b783d814bb6a52b6be7b
João Gabriel <main.joaogabrielsg@gmail.com>
2023-01-19 08:29:06 -0300

    feat(server): add route to check day habits

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -26,4 +26,45 @@ export async function appRoutes(app: FastifyInstance) {
       },
     });
   });
+
+  app.get("/day", async (request, reply) => {
+    const getDayParams = z.object({
+      date: z.coerce.date(),
+    });
+
+    const { date } = getDayParams.parse(request.query);
+
+    const parsedDate = dayjs(date).startOf("day");
+    const weekDay = dayjs(parsedDate).get("day");
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          },
+        },
+      },
+    });
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      },
+    });
+
+    const completedHabits =
+      day?.dayHabits.map((dayHabit) => dayHabit.habit_id) ?? [];
+
+    return {
+      possibleHabits,
+      completedHabits,
+    };
+  });
 }
