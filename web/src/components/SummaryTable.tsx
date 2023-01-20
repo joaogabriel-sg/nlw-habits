fff471e44ef135a1e82da612ed16823961d0b354
João Gabriel <main.joaogabrielsg@gmail.com>
2023-01-20 10:06:37 -0300

    feat(web): create summary table component

---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,24 @@
+import { HabitDay } from "./HabitDay";
+
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+
+export function SummaryTable() {
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((weekDay, index) => (
+          <div
+            key={`${weekDay}-${index}`}
+            className="text-zinc-400 font-bold text-xl h-10 w-10 flex items-center justify-center"
+          >
+            {weekDay}
+          </div>
+        ))}
+      </div>
+
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        <HabitDay />
+      </div>
+    </div>
+  );
+}
