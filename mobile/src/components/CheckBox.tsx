4941be7777cf974fc37573bb1bbb69354ad28b82
João Gabriel <main.joaogabrielsg@gmail.com>
2023-01-22 14:24:10 -0300

    fix(mobile): add semibold style to checkbox text

--- mobile/src/components/CheckBox.tsx
+++ mobile/src/components/CheckBox.tsx
@@ -27,7 +27,7 @@ export function CheckBox({ title, checked = false, ...rest }: Props) {
         <View className="w-8 h-8 rounded-lg bg-zinc-900" />
       )}
 
-      <Text className="ml-3 text-base text-white">{title}</Text>
+      <Text className="ml-3 text-base font-semibold text-white">{title}</Text>
     </TouchableOpacity>
   );
 }
