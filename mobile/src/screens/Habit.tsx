a231750b0c032a8b71f0242dd8b4cf84256c223e
João Gabriel <main.joaogabrielsg@gmail.com>
2023-01-22 14:23:23 -0300

    feat(mobile): create habit screen

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,5 +1,47 @@
-import { View } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import dayjs from "dayjs";
+import { ScrollView, Text, View } from "react-native";
+
+import { BackButton } from "../components/BackButton";
+import { CheckBox } from "../components/CheckBox";
+import { ProgressBar } from "../components/ProgressBar";
+
+interface Params {
+  date: string;
+}
 
 export function Habit() {
-  return <View className="flex-1 px-8 pt-16 bg-background"></View>;
+  const route = useRoute();
+  const { date } = route.params as Params;
+
+  const parsedDate = dayjs(date);
+  const dayOfWeek = parsedDate.format("dddd");
+  const dayAndMonth = parsedDate.format("DD/MM");
+
+  return (
+    <View className="flex-1 px-8 pt-16 bg-background">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <BackButton />
+
+        <Text className="mt-6 text-base font-semibold lowercase text-zinc-400">
+          {dayOfWeek}
+        </Text>
+
+        <Text className="text-3xl font-extrabold text-white">
+          {dayAndMonth}
+        </Text>
+
+        <ProgressBar progress={30} />
+
+        <View className="mt-6">
+          <CheckBox title="Beber 2L de água" checked={false} />
+
+          <CheckBox title="Caminhar" checked={true} />
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,4 +1,5 @@
 import { ScrollView, Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import { Header } from "../components/Header";
 import { DAY_SIZE, HabitDay } from "../components/HabitDay";
@@ -11,15 +12,17 @@ const minimumSummaryDatesSizes = 18 * 7;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
+
   return (
-    <View className="flex-1 bg-background px-8 pt-16">
+    <View className="flex-1 px-8 pt-16 bg-background">
       <Header />
 
       <View className="flex-row mt-6 mb-2">
         {weekDays.map((weekDay, index) => (
           <Text
             key={`${weekDay}-${index}`}
-            className="text-zinc-400 text-xl font-bold text-center mx-1"
+            className="mx-1 text-xl font-bold text-center text-zinc-400"
             style={{ width: DAY_SIZE }}
           >
             {weekDay}
@@ -33,14 +36,17 @@ export function Home() {
       >
         <View className="flex-row flex-wrap">
           {datesFromYearStart.map((date) => (
-            <HabitDay key={date.toISOString()} />
+            <HabitDay
+              key={date.toISOString()}
+              onPress={() => navigate("Habit", { date: date.toISOString() })}
+            />
           ))}
 
           {amountOfDaysToFill > 0 &&
             Array.from({ length: amountOfDaysToFill }).map((_, index) => (
               <View
                 key={index}
-                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                className="m-1 border-2 rounded-lg bg-zinc-900 border-zinc-800 opacity-40"
                 style={{ width: DAY_SIZE, height: DAY_SIZE }}
               />
             ))}
